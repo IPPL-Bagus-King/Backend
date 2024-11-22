7070ebd64b58c25bdcc236c119e509bef86cf289
omencakep <rakaaw3108@gmail.com>
2024-11-22 20:19:16 +0700

    feature: api delete forum

--- routes/forum.js
+++ routes/forum.js
@@ -1,34 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const forumController = require('../controllers/forumController');
-const authenticateUser = require('../middleware/authenticateUser');
-const authorizeTeacher = require('../middleware/authorizeTeacher');
+const auth = require('../middleware/authenticateUser');
+const authTeacher = require('../middleware/authorizeTeacher');
 
 // route create forum
-router.post(
-  '/',
-  authenticateUser,
-  authorizeTeacher,
-  forumController.createForum
-);
+router.post('/', auth, authTeacher, forumController.createForum);
 
 // route get forums
-router.get('/', authenticateUser, forumController.getForums);
+router.get('/', auth, forumController.getForums);
 
 // route get forums by teacher id
 router.get(
   '/teacher-forums',
-  authenticateUser,
-  authorizeTeacher,
+  auth,
+  authTeacher,
   forumController.getForumsByTeacherId
 );
 
 // route update forum
-router.put(
-  '/:id',
-  authenticateUser,
-  authorizeTeacher,
-  forumController.updateForum
-);
+router.put('/:id', auth, authTeacher, forumController.updateForum);
+
+// route delete forum
+router.delete('/:id', auth, authTeacher, forumController.deleteForum);
 
 module.exports = router;
